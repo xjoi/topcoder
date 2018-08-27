43a44b3bb48fb59726d4a943b98468746dd76ecf
becauseofyou <593572013@qq.com>
2018-08-27 18:45:46 +0800

    topcoder

---
+++ template.cpp
@@ -0,0 +1,20 @@
+//tc is healthy, just do it
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> void checkmin(T &a,const T &b) { if (b<a) a=b; } 
+template<class T> void checkmax(T &a,const T &b) { if (b>a) a=b; }
+
+class $CLASSNAME$ {
+public:
+    $RC$ $METHODNAME$( $METHODPARMS$ ) ;
+};
+$RC$ $CLASSNAME$::$METHODNAME$($METHODPARMS$) {
+    
+}
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
